8de218ea341f4d58c4d9528b3ee2179d3c5fe096
Piyushpal01 <palp2406@gmail.com>
2025-07-28 18:06:51 +0530

    fixed build.sh file

--- build.sh
+++ build.sh
@@ -6,5 +6,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input   # Django ki ek built-in command hai jo saare static files ko ek central location (usually static/ folder) mein collect kar deti hai.
 python manage.py migrate
-
-python manage.py shell < create_superuser.py
