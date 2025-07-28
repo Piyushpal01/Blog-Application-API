b326c7631850721b5cbd9365e6051d4a1e39f39b
Piyushpal01 <palp2406@gmail.com>
2025-07-28 15:48:07 +0530

    Fixed build.sh file

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 
 set -o errexit  # exit on error
 
-cd blog_app
-
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input   # Django ki ek built-in command hai jo saare static files ko ek central location (usually static/ folder) mein collect kar deti hai.
