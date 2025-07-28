18898ce359c3ac4d37d26b77a4dd14ee9cfc8def
Piyushpal01 <palp2406@gmail.com>
2025-07-28 22:35:49 +0530

    Fixed issues

--- blog_app/settings.py
+++ blog_app/settings.py
@@ -62,8 +62,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# ✅ Allow all origins
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://inkspire-p7wi.onrender.com"
+]
 
 ROOT_URLCONF = 'blog_app.urls'
 
